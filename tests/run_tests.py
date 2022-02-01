c5af22ab824cf7ba1c6d9cd83286eb467ebf481c
Johannes Schöck <38401818+JSchoeck@users.noreply.github.com>
2022-02-01 08:46:54 +0100

    Update run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,1 +1,14 @@
 # Unit tests
+import unittest
+
+class SimpleTest(unittest.TestCase):
+  
+    def test(self):        
+        self.assertTrue(True)
+
+    def test_run_stuff(self):
+        self.assertEqual(run_stuff(1), 2)
+        
+if __name__ == '__main__':
+  unittest.main()
+  test_run_stuff()
